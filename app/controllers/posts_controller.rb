1e8ab0a11a279748e0d41d38cd413e0d808cde96
Owen Ou <matsu727@me.com>
2016-06-17 15:58:07 +0800

    CRUD done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,49 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, :only => [:show, :edit, :update, :destroy]
+
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @page_title = @post.title
+  end
+
+  def create
+    @post = Post.new( post_params )
+    if @post.save 
+      flash[:notice] = "New Post Added"
+      redirect_to posts_path 
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @page_title = @post.title
+  end
+
+  def update  
+   if @post.update(post_params)
+      flash[:notice] = "Information Updated "
+      redirect_to post_path(@post)
+    else
+      render :action => :edit
+    end 
     
   end
 
+  def destroy
+    @post.delete
+    flash[:alert] = "Post Deleted"
+    redirect_to posts_path
+  end  
 
 
 
@@ -14,11 +53,15 @@ class PostsController < ApplicationController
 
 
 
+private
+    
+  def post_params
+      params.require(:post).permit(:title, :description)
+  end
 
-
-
-
-
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
 
 
